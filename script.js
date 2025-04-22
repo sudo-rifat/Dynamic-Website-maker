e4e7eeed8d465d3155548e166c1dcc4130725b1d
MD. MEHEDI HASSAN RIFAT <mdmehedihassanrifat3@gmail.com>
2025-04-22 16:25:50 +0600

    Update script.js
    added functionality to add dynamic boxes unlimited and make elements dynamically

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
-let box = document.querySelector(".box").children
 
-let thumbnail = document.querySelector(".image")
-let title = document.querySelector(".title")
-let channel = document.querySelector(".channel")
-let views = document.querySelector(".views")
-let months = document.querySelector(".months") 
-let duration = document.querySelector(".duration")
+//    view counter by kilo billion million trillion
+function views_func(vc) {
+
+    if ((vc >= 1000) && (vc < 1000000)) {
+        vc = vc / 1000;
+        vc = vc.toFixed(2);
+        vc = String(`${vc}K`);
+        console.log(vc);
+    }    
+    else if ((vc >= 1000000) && (vc < 1000000000)) {
+        vc = vc / 1000000;
+        vc = vc.toFixed(2);
+        vc = String(`${vc}M`);
+        console.log(vc);
+    }    
+    else if ((vc >= 1000000000) && (vc < 1000000000000)) {
+        vc = vc / 1000000000;
+        vc = vc.toFixed(2);
+        vc = String(`${vc}B`);
+        console.log(vc);
+    }    
+    else if (vc >= 1000000000000) {
+        vc = vc / 1000000000000;
+        vc = vc.toFixed(2);
+        vc = String(`${vc}T`);
+        console.log(vc);
+    }    
+
+    return vc;
+}    
 
 
-title.innerText = prompt("Enter Title : ")
-channel.innerText = prompt("Enter Channel name : ")
-let views_counter = prompt("Enter views : ");
-months.innerText = prompt("Enter months : ")
-duration.innerText = prompt("Enter duration : ")
-thumbnail.setAttribute("src",prompt("Enter image link"))
+
+
+//      taking user input
+function take_input(i) {
+    document.querySelectorAll(".box")[i].querySelector(".title").innerText = prompt("Enter Title : ");
+    // document.querySelectorAll(".box")[0].querySelector(".title").innerHTML = "Hello World";
+    document.querySelectorAll(".box")[i].querySelector(".channel").innerText = prompt("Enter Channel name : ");
+    document.querySelectorAll(".box")[i].querySelector(".months").innerText = `${prompt("Enter months : ")}months ago`;
+    document.querySelectorAll(".box")[i].querySelector(".duration").innerText = prompt("Enter duration : ");
+    document.querySelectorAll(".box")[i].querySelector(".image").setAttribute("src", prompt("Enter image link"));
+    document.querySelectorAll(".box")[i].querySelector(".views").innerText = `${views_func(Number(prompt("Enter views : ")))} views ` ; //  view এর সংখ্যা ইনপুট নেবে (স্ট্রিং হিশেবে থাকবে ইনপুট) - স্ট্রিং কে নাম্বার বানাবে - views_func নামের function এ তা ইনপুট দেবে - সেখান থেকে K, M, B, T সহ লেখা ফিরে আসবে - innerText এ সেট হবে
+}    
 
 
 
@@ -23,35 +52,41 @@ thumbnail.setAttribute("src",prompt("Enter image link"))
 
 
 
+//     Declarations
+const temp_box = (`
+
+    <section class="thumbnail">
+        <img src="" alt="Thumbnail" class="image">
+        <div class="duration"></div>
+    </section>
+
+    <section class="text">
+        <div class="title"></div>
+        <div class="undertitle">
+            <span class="channel"></span>•
+            <span class="views"></span>•
+            <span class="months"></span>
+        </div>
+    </section>
+
+`)
 
 
-let vc = views_counter;
-vc = + vc;
 
 
-if ((vc >= 1000) && (vc < 1000000)) {
-    vc = vc / 1000;
-    vc = vc.toFixed(2);
-    vc = String(`${vc}K`);
-    console.log(vc);
-} 
-else if ((vc >= 1000000) && (vc < 1000000000)) {
-    vc = vc / 1000000;
-    vc = vc.toFixed(2);
-    vc = String(`${vc}M`);
-    console.log(vc);
-} 
-else if ((vc >= 1000000000) && (vc < 1000000000000)) {
-    vc = vc / 1000000000;
-    vc = vc.toFixed(2);
-    vc = String(`${vc}B`);
-    console.log(vc);
-} 
-else if (vc >= 1000000000000) {
-    vc = vc / 1000000000000;
-    vc = vc.toFixed(2);
-    vc = String(`${vc}T`);
-    console.log(vc);
-} 
 
-views.innerText = vc
+
+
+// main function 
+for(i=0;prompt("Stop ! [Y/N]") === "Y";i++){
+    elem = document.createElement("div");
+    elem.className = "box"
+    elem.innerHTML = temp_box;
+    document.querySelector(".container").append(elem);
+    take_input(i); 
+
+    // এইখানে অনেক্ষন আটকাইয়া ছিলাম 
+    // কারন ঃ append করার আগে input নিতে চাইছিলাম
+    // শিক্ষা ঃ input নেয়ার আগে element insert করতে হবে
+
+}
